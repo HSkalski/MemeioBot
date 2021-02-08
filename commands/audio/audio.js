1b63535a1108e82aecf34371f706510f51a068f2
HSkalski <harrison.skalski@gmail.com>
2021-02-08 09:22:52 -0600

    ability to 'send' audio to a channel if you aren't connected

--- commands/audio/audio.js
+++ commands/audio/audio.js
@@ -29,11 +29,11 @@ let play = (message, time) => {
 
 module.exports = {
     name:'audio',
-    description:'queue: lists q, skip: skips current play, stop: clear queue',
-    usage:'\'queue\' | \'skip\'|\'stop\'',
+    description:'queue: lists q, skip: skips current play, stop: clear queue, send: sends to specified channel',
+    usage:'\'queue\' | \'skip\'|\'stop\' \n|\'send\'|',
     execute(message, args){ // called from bot.js
         let server = servers[message.guild.id];
-        switch(args.join(' ')){
+        switch(args[0]){
             // case 'play': // for testing, will eventually be all internal
             //     this.playAudioFile(message, args[1]);
             //     break;
@@ -58,8 +58,27 @@ module.exports = {
                 message.channel.send(servers[message.guild.id].queue);
             case 'leave':
                 //let server = servers[message.guild.id];
+                //console.log(server);
                 server.connection.disconnect();
                 //console.log(server.connection);
+                break;
+            case 'send':
+                args.shift();
+                //console.log(args.join(' '));
+                if(!servers[message.guild.id]) servers[message.guild.id] = { // add the server if they aren't in
+                    queue: [],
+                    connection: undefined,
+                    playing: false,
+                    sendAudio: undefined
+                }
+                server = servers[message.guild.id];
+                if(!args[0] && server.sendAudio){
+                    message.reply(`Sending audio to ${server.sendAudio.name}`)
+                }
+                if(message.guild.channels.cache.find(c => c.name === args.join(' '))){
+                    server.sendAudio = message.guild.channels.cache.find(c => c.name === args.join(' '));
+                }
+                //console.log( message.guild.channels.cache.find(c => c.name === args.join(' ')));
                 break;
             default:
                 break;
@@ -71,19 +90,27 @@ module.exports = {
             message.channel.send("Need a file to play bub");
             return;
         }
-        if(!message.member.voice.channel){
-            message.reply("ya gotta join a voice channel first dummy");
-            return;
-        }
         if(!servers[message.guild.id]) servers[message.guild.id] = {
             queue: [],
             connection: undefined,
-            playing: false
+            playing: false,
+            sendAudio: undefined
+        }
+        if(!message.member.voice.channel ){
+            if(!servers[message.guild.id].sendAudio){
+                message.reply("ya gotta join a voice channel first or use 'send' dummy");
+                return;
+            }
         }
         var server = servers[message.guild.id];
         server.queue.push(path);
         console.log(`queue: ${server.queue}`);
-        let voiceChannel = message.member.voice.channel;
+        let voiceChannel = undefined;
+        if(message.member.voice.channel){
+            voiceChannel = message.member.voice.channel;
+        }else{
+            voiceChannel = server.sendAudio;
+        }
         // console.log(message.client.voice.connections.get(voiceChannel.guild.id));
         // console.log(voiceChannel.guild.id);
         //console.log(!server.connection);
